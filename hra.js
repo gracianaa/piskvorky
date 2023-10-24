a5949594fc363883826b3854c9699f5222b4c7b0
Graciána Adamcová <adamcova.grace@gmail.com>
2023-10-24 16:30:42 +0200

    požadavek na API, získanie navrhovaného tahu (x) a zavolanie metody click

--- hra.js
+++ hra.js
@@ -17,7 +17,7 @@ const changePlayer = () => {
 // pridanie const herniPole a funkciu, ktorá pridáva value daného buttnu do tohto poľa
 // pridanie const vitez a funkcie findWinner
 
-const selectBtn = (event) => {
+const selectBtn = async (event) => {
   if (currentPlayer === 'o') {
     event.target.classList.add('board__field--circle');
     event.target.disabled = true;
@@ -32,7 +32,8 @@ const selectBtn = (event) => {
 
   const herniPole = [];
 
-  document.querySelectorAll('.game__board-btn').forEach((buttonSign) => {
+  const fields = document.querySelectorAll('.game__board-btn');
+  fields.forEach((buttonSign) => {
     let hodnota = buttonSign.value;
     if (hodnota === '') {
       hodnota = '_';
@@ -50,6 +51,32 @@ const selectBtn = (event) => {
   } else if (vitez === 'tie') {
     alert(`Remíza`);
   }
+
+  //Úkol: Piškvorky 5/5
+  //odešli požadavek na API a získej navrhovaný tah. API běží na adrese https://piskvorky.czechitas-podklady.cz/api/suggest-next-move a očekává metodu POST, hlavičku Content-type: application/json a tělo ve formátu JSON. V JSONu musí být objekt s vlastností board, kde hodnota je stejné pole, jako ve volání funkce findWinner z předchozího úkolu. Objekt dále musí obsahovat vlastnost player s hodnotou 'x' vzhledem k tomu, že chceme návrh tahu pro křížek.
+
+  const response = await fetch(
+    'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
+    {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        board: herniPole,
+        player: 'x',
+      }),
+    },
+  );
+
+  const data = await response.json();
+  // console.log(data);
+  const { x, y } = data.position;
+  // x bude 0 a y bude 1, protože to je jediné volné políčko. x 0 odpovídá prvnímu sloupci a y 1 druhému řádku.
+  const field = fields[x + y * 10];
+  // Najde políčko na příslušné pozici.
+  if (currentPlayer === 'x') {
+    field.click();
+  }
+  // Simuluje kliknutí. Spustí událost `click` na políčku.
 };
 
 const btn = document.querySelectorAll('.game__board-btn');
